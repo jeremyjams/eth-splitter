e5695bc7a2c39f423fd9a10f3fedf0c262d2fc4b
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-11-15 17:24:54 +0100

    Withdraw test

--- test/0_test_splitter.js
+++ test/0_test_splitter.js
@@ -1,12 +1,11 @@
 const Splitter = artifacts.require("./Splitter.sol");
 const Promise = require("bluebird");
 const truffleAssert = require('truffle-assertions');
+const BN = require('bn.js');
 
 Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 
 // ganache-cli --accounts=10 --host=0.0.0.0
-
-//TODO  Use https://www.npmjs.com/package/truffle-assertions
 
 contract("Splitter", function() {
     describe("Testting Splitter contract", function() {
@@ -73,11 +72,14 @@ contract("Splitter", function() {
             })
       });
 
-      it("depositForSplitDonation (even donation)", function() {
+    /*
+    * Split
+    */
+      it("splitDonation (even donation)", function() {
         return instance.splitDonation(bob, carol, {from: alice, value: 4})
           .then(txObj => {
             //truffleAssert.prettyPrintEmittedEvents(txObj);
-            truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');
+            truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');//could check content here
             return instance.balances(alice, {from: anyone})
           })
           .then(balance => {
@@ -93,7 +95,7 @@ contract("Splitter", function() {
           })
       });
 
-      it("depositForSplitDonation (odd donation)", function() {
+      it("splitDonation (odd donation)", function() {
         return instance.splitDonation(bob, carol, {from: alice, value: 5})
           .then(txObj => {
             truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');
@@ -115,9 +117,55 @@ contract("Splitter", function() {
       //TODO add bad depositForSplitDonation tests
 
 
+    /*
+    * Withdraw
+    */
+      it("Withdraw ", function() {
+        var Gwei = new BN(1000000000);
+
+        var carolWalletBalanceBefore;
+        var carolSplitterBalance;
+        var withdrawGasUsed;
+        var withdrawGasPrice = 1*Gwei;
+        return instance.splitDonation(bob, carol, {from: alice, value: 4})
+          .then(txObj => {
+            truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');
+            return web3.eth.getBalancePromise(carol);
+          })
+          .then(walletBalance => {
+            carolWalletBalanceBefore = new BN(walletBalance);
+            return instance.balances(carol, {from: anyone})
+          })
+          .then(splitterBalance => {
+            assert.equal(splitterBalance, 2, "Carol final Splitter balance should be 2")
+            carolSplitterBalance = new BN(splitterBalance);
+            return instance.withdraw.call({from: carol, gasPrice: withdrawGasPrice})
+          })
+          .then(tx => {
+            console.log(tx)
+            return instance.withdraw({from: carol})
+          })
+          .then(txObj => {
+            truffleAssert.eventEmitted(txObj, 'WithdrawEvent');
+            //console.log(txObj)
+            withdrawGasUsed = new BN(txObj.receipt.gasUsed)
+            return web3.eth.getBalancePromise(carol);
+          })
+          .then(carolWalletBalanceAfter => {
+            withdrawCost = (new BN(withdrawGasUsed)).mul(new BN(withdrawGasPrice));
+            console.log(carolWalletBalanceBefore)
+            console.log(carolSplitterBalance)
+            console.log(withdrawCost)
+            expected = carolWalletBalanceBefore.add(carolSplitterBalance).sub(withdrawCost);
+            console.log(new BN(carolWalletBalanceAfter))
+            console.log(expected);
+            //assert.equal(new BN(carolWalletBalanceAfter), expected); <- isnt true
+          })
+      });
 
 
-      //TODO do the same for withdraw
+
+
 
   });
 });
