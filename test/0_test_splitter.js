7c099b616000b786ed7c02c8a480399382351443
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-12-23 17:34:41 +0100

    Updated all tests with async/await

--- test/0_test_splitter.js
+++ test/0_test_splitter.js
@@ -10,108 +10,73 @@ Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 contract("Splitter", function() {
     describe("Testting Splitter contract", function() {
 
-        let instance;
-        let owner;
+        let splitter;
         let alice;
         let bob;
         let carol;
         let anyone;
 
-        beforeEach("Fresh contract & accounts", function() {
-            return Splitter.new(false)
-            .then(_instance => {
-            instance = _instance;
-            return instance.getOwner();
-            })
-            .then(_owner => {
-            owner = _owner;
-            console.log(owner + ": owner address");
-            return web3.eth.getAccountsPromise();
-            })
-            .then(accounts => {
+        beforeEach("Fresh contract & accounts", async function() {
+            splitter = await Splitter.new(false)
+
+            let accounts = await web3.eth.getAccountsPromise();
             alice = accounts[1];
             bob = accounts[2];
             carol = accounts[3];
             anyone = accounts[9];
 
-            console.log(alice + ": alice address");
-            console.log(bob + ": bob address");
-            console.log(carol + ": carol address");
+            let aliceWeiBalance = await web3.eth.getBalancePromise(alice)
+            let aliceBalance = await web3.utils.fromWei(aliceWeiBalance)
+            assert.isTrue(aliceBalance.toString(10) > "10", "Alice is broke");
 
-            return true;
-            })
-            .then(success => web3.eth.getBalancePromise(alice))
-            .then(weiBalance => web3.utils.fromWei(weiBalance))
-            .then(balance => {
-            console.log(balance + " ETH: alice balance");
-            return assert.isTrue(balance.toString(10) > "10", "Alice is broke");
-            })
-            .then(success => web3.eth.getBalancePromise(bob))
-            .then(weiBalance => web3.utils.fromWei(weiBalance))
-            .then(balance => {
-            console.log(balance + " ETH: bob balance");
-            return assert.isTrue(balance.toString(10) > "10", "Bob is broke");
-            })
-            .then(success => web3.eth.getBalancePromise(carol))
-            .then(weiBalance => web3.utils.fromWei(weiBalance))
-            .then(balance => {
-            console.log(balance + " ETH: carol balance");
-            return assert.isTrue(balance.toString(10) > "10", "Carol is broke");
-            })
-            .then(success => instance.balances.call(alice, {from: anyone}))
-            .then(balance => {
-            assert.equal(balance, 0, "Alice initial Splitter balance should be 0")
-            return instance.balances(bob, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 0, "Bob initial Splitter balance should be 0")
-            return instance.balances(carol, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 0, "Carol initial Splitter balance should be 0")
-            })
+            let bobWeiBalance = await web3.eth.getBalancePromise(bob)
+            let bobBalance = await web3.utils.fromWei(bobWeiBalance)
+            assert.isTrue(bobBalance.toString(10) > "10", "Bob is broke");
+
+            let carolWeiBalance = await web3.eth.getBalancePromise(carol)
+            let carolBalance = await web3.utils.fromWei(carolWeiBalance)
+            assert.isTrue(carolBalance.toString(10) > "10", "Carol is broke");
+
+            let aliceSplitterBalance = await splitter.balances.call(alice, {from: anyone})
+            assert.equal(aliceSplitterBalance, 0, "Alice initial Splitter balance should be 0")
+
+            let bobSplitterBalance = await splitter.balances.call(bob, {from: anyone})
+            assert.equal(bobSplitterBalance, 0, "Bob initial Splitter balance should be 0")
+
+            let carolSplitterBalance = await splitter.balances.call(carol, {from: anyone})
+            assert.equal(carolSplitterBalance, 0, "Carol initial Splitter balance should be 0")
         });
 
         /*
         * Split
         */
-        it("splitDonation (even donation)", function() {
-            return instance.splitDonation(bob, carol, {from: alice, value: 4})
-            .then(txObj => {
+        it("splitDonation (even donation)", async function() {
+            let txObj = await  splitter.splitDonation(bob, carol, {from: alice, value: 4})
             //truffleAssert.prettyPrintEmittedEvents(txObj);
             truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');//could check content here
-            return instance.balances(alice, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 0, "Alice final Splitter balance should be 0")
-            return instance.balances(carol, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 2, "Bob final Splitter balance should be 2")
-            return instance.balances(carol, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 2, "Carol final Splitter balance should be 2")
-            })
+
+            let aliceBalance = await splitter.balances(alice, {from: anyone})
+            assert.equal(aliceBalance, 0, "Alice final Splitter balance should be 0")
+
+            let bobBalance = await splitter.balances(bob, {from: anyone})
+            assert.equal(bobBalance, 2, "Bob final Splitter balance should be 2")
+
+            let carolBalance = await splitter.balances(carol, {from: anyone})
+            assert.equal(carolBalance, 2, "Carol final Splitter balance should be 2")
         });
 
-        it("splitDonation (odd donation)", function() {
-            return instance.splitDonation(bob, carol, {from: alice, value: 5})
-            .then(txObj => {
-            truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');
-            return instance.balances(alice, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 1, "Alice final Splitter balance should be 1")
-            return instance.balances(carol, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 2, "Bob final Splitter balance should be 2")
-            return instance.balances(carol, {from: anyone})
-            })
-            .then(balance => {
-            assert.equal(balance, 2, "Carol final Splitter balance should be 2")
-            })
+        it("splitDonation (odd donation)", async function() {
+            let txObj = await  splitter.splitDonation(bob, carol, {from: alice, value: 5})
+            truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');//could check content here
+
+            let aliceBalance = await splitter.balances(alice, {from: anyone})
+            assert.equal(aliceBalance, 1, "Alice final Splitter balance should be 1")
+
+            let bobBalance = await splitter.balances(bob, {from: anyone})
+            assert.equal(bobBalance, 2, "Bob final Splitter balance should be 2")
+
+            let carolBalance = await splitter.balances(carol, {from: anyone})
+            assert.equal(carolBalance, 2, "Carol final Splitter balance should be 2")
         });
 
         //TODO add bad depositForSplitDonation tests
@@ -121,18 +86,18 @@ contract("Splitter", function() {
         * Withdraw
         */
         it("Withdraw ", async function() {
-            let txObj = await instance.splitDonation(bob, carol, {from: alice, value: 4});
+            let txObj = await splitter.splitDonation(bob, carol, {from: alice, value: 4});
             truffleAssert.eventEmitted(txObj, 'SplitDonationEvent');
 
             //wallet before
             let balanceBefore = await web3.eth.getBalancePromise(carol);
 
             //top up some funds
-            let splitterBalance = await instance.balances(carol, {from: anyone})
+            let splitterBalance = await splitter.balances(carol, {from: anyone})
             assert.equal(splitterBalance, 2, "Carol final Splitter balance should be 2")
 
             //lets withdraw
-            let receipt = await instance.withdraw({from: carol})
+            let receipt = await splitter.withdraw({from: carol})
             let gasUsed = receipt.receipt.gasUsed;
             let tx = await web3.eth.getTransaction(receipt.tx);
             let gasPrice = tx.gasPrice;
